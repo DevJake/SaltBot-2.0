eff588bbc5c0cfcb94e877e2746cd0e7aa28b1c0
Salt001 <salt.exe@gmail.com>
2017-04-14 22:17:02 +0100

    Revert "Changed author to a User-type"
    This reverts commit 42803b33b2b032554aea1e69f66435797c7dc800.

--- src/me/Salt/Command/CommandDescription.java
+++ src/me/Salt/Command/CommandDescription.java
@@ -1,29 +1,26 @@
 package me.Salt.Command;
 
 import me.Salt.Permissions.Permission;
-import net.dv8tion.jda.core.entities.User;
 
 import java.util.HashMap;
 import java.util.List;
 
 /**
- * ->> SaltBot-2.0 <<-
- * Created by Salt on 09/04/2017.
+ * Created by Salt001 on 09/04/2017.
  */
-public class CommandDescription {
-    private HashMap<String, Boolean> parameters;
-    private HashMap<String, Boolean> definers;
+class CommandDescription {
+    private final HashMap<String, Boolean> parameters;
+    private final HashMap<String, Boolean> definers;
     private String name;
     private String description;
-    private User author;
+    private String author;
     private boolean isComplete;
     private boolean supportsPermissions;
     private List<Permission> requiredPermissions;
     private String helpMessage;
-    private boolean deprecated;
-    private List<String> commandCallers; //What text is to be identified by the program as pointing to this command
 
-    public CommandDescription(HashMap<String, Boolean> parameters, HashMap<String, Boolean> definers, String name, String description, User author, boolean isComplete, boolean supportsPermissions, List<Permission> requiredPermissions, String helpMessage, boolean deprecated, List<String> commandCallers) {
+
+    public CommandDescription(HashMap<String, Boolean> parameters, HashMap<String, Boolean> definers, String name, String description, String author, boolean isComplete, boolean supportsPermissions, List<Permission> requiredPermissions, String helpMessage) {
         this.parameters = parameters;
         this.definers = definers;
         this.name = name;
@@ -33,52 +30,6 @@ public class CommandDescription {
         this.supportsPermissions = supportsPermissions;
         this.requiredPermissions = requiredPermissions;
         this.helpMessage = helpMessage;
-        this.deprecated = deprecated;
-        this.commandCallers = commandCallers;
-    }
-
-    public HashMap<String, Boolean> getParameters() {
-        return parameters;
-    }
-
-    public HashMap<String, Boolean> getDefiners() {
-        return definers;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public User getAuthor() {
-        return author;
-    }
-
-    public boolean isComplete() {
-        return isComplete;
-    }
-
-    public boolean isSupportsPermissions() {
-        return supportsPermissions;
-    }
-
-    public List<Permission> getRequiredPermissions() {
-        return requiredPermissions;
-    }
-
-    public String getHelpMessage() {
-        return helpMessage;
-    }
-
-    public boolean isDeprecated() {
-        return deprecated;
-    }
-
-    public List<String> getCommandCallers() {
-        return commandCallers;
     }
 
     @Override
@@ -86,7 +37,7 @@ public class CommandDescription {
         return "CommandDescription{" +
                 "name='" + name + '\'' +
                 ", description='" + description + '\'' +
-                ", author='" + author.getName() + '\'' +
+                ", author='" + author + '\'' +
                 ", helpMessage='" + helpMessage + '\'' +
                 '}';
     }
