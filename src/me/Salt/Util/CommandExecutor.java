5cf7ee26daa69ec910a30932ca22769bac53d6e7
Salt001 <salt.exe@gmail.com>
2017-04-14 22:21:23 +0100

    Changes to CommandExecutor
    Added an algorithm to #addCommand(), to ensure that the CommandCallers
    in the command to-add are not already in use by pre-existing commands

--- src/me/Salt/Util/CommandExecutor.java
+++ src/me/Salt/Util/CommandExecutor.java
@@ -1,13 +1,21 @@
 package me.Salt.Util;
 
+import me.Salt.Command.CommandContainer;
 import me.Salt.Command.ICommand;
 import me.Salt.Container.CommandParser;
+import me.Salt.Exception.DuplicateDataException;
 import me.Salt.Exception.MalformedParametersException;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Manages the execute cycle of a command
  */
 public class CommandExecutor {
+    private static List<CommandContainer> commands = new ArrayList<>();
+
     public static void execute(ICommand command, CommandParser.CommandContainer cmd) throws MalformedParametersException {
 
         Executor ex = (c, d) -> {
@@ -19,6 +27,19 @@ public class CommandExecutor {
         };
 
         ex.execute(command, cmd);
+    }
+
+    public static void addCommand(CommandContainer command) throws DuplicateDataException {
+        //TODO make sure Commands cannot have duplicate commandCallers, either in the same command or different commands.
+        if (commands != null) {
+            if (command.getCommandDescription().getCommandCallers().stream().filter(s -> commands.stream().filter(c -> c.getCommandDescription().getCommandCallers().contains(s)).collect(Collectors.toList()).size() > 0).collect(Collectors.toList()).size() > 0) {
+                throw new DuplicateDataException("Command Callers specified for \"" + command.getCommandDescription().getName() + "\" are overlapping with alternate Command Callers from different Commands!");
+            } else {
+                commands.add(command);
+            }
+        } else {
+            commands.add(command);
+        }
     }
 
     private interface Executor {
