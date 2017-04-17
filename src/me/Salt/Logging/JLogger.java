ec6becfc2ec238c01afd6a40736c70dea8126e27
Salt001 <salt.exe@gmail.com>
2017-04-18 00:14:30 +0100

    Added some new methods and logging types
    -Ideally will be changing in the future for a simpler system
    -Also added a 'DEBUG' log-level

--- src/me/Salt/Logging/JLogger.java
+++ src/me/Salt/Logging/JLogger.java
@@ -18,6 +18,7 @@ package me.Salt.Logging;
 
 import java.time.Instant;
 import java.time.ZoneOffset;
+import java.util.List;
 
 /**
  * Allows for the logging of information to the console, text file, or both. Currently supports just console logging.
@@ -66,6 +67,41 @@ public class JLogger {
         System.out.println("[" + Instant.now().atOffset(ZoneOffset.ofHours(-1)) + "]" + " [" + level.name() + "] " + message + ". Thrown by class: " + o.getClass().getName());
     }
 
+    public static void writeToConsole(List<String> prefixes, Level level, String message, Object o) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("[" + Instant.now().atOffset(ZoneOffset.ofHours(-1)) + "] [" + level.name() + "] ");
+        prefixes.forEach(p -> sb.append("[" + p + "]"));
+        sb.append(message + ". Thrown by class: " + o.getClass().getName());
+        System.out.println(sb.toString());
+    }
+
+    public static void writeToConsole(String prefix, Level level, String message, Object o) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("[" + Instant.now().atOffset(ZoneOffset.ofHours(-1)) + "] [" + level.name() + "] ");
+        sb.append("[" + prefix + "]");
+        sb.append(message + ". Thrown by class: " + o.getClass().getName());
+        System.out.println(sb.toString());
+    }
+
+    public static void writeToConsole(List<String> prefixes, Level level, String message) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("[" + Instant.now().atOffset(ZoneOffset.ofHours(-1)) + "][" + level.name() + "]");
+        prefixes.forEach(p -> sb.append("[" + p + "]"));
+        sb.append(" "); //To 'sexify' the output
+        sb.append(message);
+        System.out.println(sb.toString());
+    }
+
+    public static void writeToConsole(String prefix, Level level, String message) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("[" + Instant.now().atOffset(ZoneOffset.ofHours(-1)) + "][" + level.name() + "]");
+        sb.append("[" + prefix + "] ");
+        sb.append(message);
+        System.out.println(sb.toString());
+    }
+
+//TODO Change to a simple method with a message, or a method with a LogBuilder. Simplifies the process of making log entries.
+
     /**
      * The level of the warning that should be used when creating log and/or console entries.
      * <p>
@@ -78,13 +114,16 @@ public class JLogger {
      * SEVERE - A notice of extremely improper - potentially crash-causing - behaviour
      * <br>
      * FATAL - A notice of crash-causing details; often composing the final set of entries to a log file
+     * <br>
+     * DEBUG - Information displayed whilst debugging mode is enabled
      */
     public enum Level {
         INFO, //Information and details of generic operation
         CONFIG, //Information about changes related to configurations
         WARNING, //A notice of improper behaviour in the application
         SEVERE, //A notice of extremely improper - potentially crash-causing - behaviour
-        FATAL //A notice of crash-causing details; often composing the final set of entries to a log file
+        FATAL, //A notice of crash-causing details; often composing the final set of entries to a log file
+        DEBUG //Information displayed when debugging mode is enabled
     }
 
 
