bc348e77f2f28de9f3f6f6e3665d899584d9454d
Salt001 <salt.exe@gmail.com>
2017-04-21 22:00:43 +0100

    Added data to JGuildImpl
    - Also added a JGuildBuilder

--- src/me/Salt/SaltAPI/Guild/JGuild.java
+++ src/me/Salt/SaltAPI/Guild/JGuild.java
@@ -16,11 +16,31 @@
 
 package me.Salt.SaltAPI.Guild;
 
+import me.Salt.Exception.DuplicateDataException;
+import me.Salt.Permissions.Perm;
 import net.dv8tion.jda.core.entities.Guild;
+import net.dv8tion.jda.core.entities.TextChannel;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 /**
  * Created by Salt001 on 10/04/2017.
  */
 public interface JGuild {
     public Guild getGuild();
+
+    public long getGuildId();
+
+    public JGuild addPermission(Perm permission) throws DuplicateDataException;
+
+    public JGuild removePermission(Perm permission);
+
+    public List<Perm> getPermissions();
+
+    public String getLastMessage();
+
+    public LocalDateTime getLastActivity();
+
+    public TextChannel getLastTextChannelMessaged();
 }
