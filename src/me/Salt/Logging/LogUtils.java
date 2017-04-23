6b337afdd84afd0ce34dab1172f7dce8f557f210
Salt001 <salt.exe@gmail.com>
2017-04-23 01:02:00 +0100

    Made plans for a rework of the logging system

---
+++ src/me/Salt/Logging/LogUtils.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright (c) 2017 DevJake
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package me.Salt.Logging;
+
+import net.dv8tion.jda.core.entities.Guild;
+
+public class LogUtils {
+
+    public static void info(String message) {
+
+    }
+
+    public static void config(String message) {
+
+    }
+
+    public static void warning(String message) {
+
+    }
+
+    public static void severe(String message) {
+
+    }
+
+    public static void fatal(String message) {
+
+    }
+
+    public static void debug(String message) {
+
+    }
+
+    public static void info(String message, Guild guild) {
+
+    }
+
+    public static void config(String message, Guild guild) {
+
+    }
+
+    public static void warning(String message, Guild guild) {
+
+    }
+
+    public static void severe(String message, Guild guild) {
+
+    }
+
+    public static void fatal(String message, Guild guild) {
+
+    }
+
+    public static void debug(String message, Guild guild) {
+
+    }
+}
