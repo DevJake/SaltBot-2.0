4b29bef72cd74a89f67af4621e4a4ab9c98b0576
Salt001 <salt.exe@gmail.com>
2017-04-26 21:45:33 +0100

    Continued development for stat-tracking command

--- src/me/Salt/Util/Utility/StatGrabber/Rainbow6/R6Handler.java
+++ src/me/Salt/Util/Utility/StatGrabber/Rainbow6/R6Handler.java
@@ -28,6 +28,15 @@ public class R6Handler {
     private List<R6Player> players = new ArrayList<>();
     private HashMap<String, R6Player> idPlayerLookup = new HashMap<>(); //Lookup by ID
     private HashMap<String, R6Player> namePlayerLookup = new HashMap<>(); //Lookup by name
+    private String apiURL;
+
+    public R6Handler(String apiURL) {
+        this.apiURL = apiURL; //https://api.r6stats.com/api/v1
+    }
+
+    public String getApiURL() {
+        return apiURL;
+    }
 
     public R6Player getPlayerById() {
         //TODO
