dc4804f473feeb0db13282a20e7fc1daeb80da9f
Salt001 <salt.exe@gmail.com>
2017-05-22 21:53:03 +0100

    Added many new events to the event firing system
    - CommandExecuteEvent
    - CommandPostExecuteEvent
    - CommandExecuteEvent
    - CommandPreExecuteEvent
    - CommandRegisterEvent
    - PermissionRegisterEvent
    - LanguageRegisterEvent
    - SaltShutdownEvent
    - SaltStartupEvent

---
+++ src/main/java/me/Salt/Event/jevent/CommandExecuteEvent.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2017 DevJake
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package me.Salt.Event.jevent;
+
+import me.Salt.Command.Container.CommandParser;
+import me.Salt.Event.jevent.util.JEvent;
+import net.dv8tion.jda.core.events.Event;
+
+public class CommandExecuteEvent extends JEvent {
+    private CommandParser.ParsedCommandContainer cmd;
+    private Event event;
+
+    public CommandExecuteEvent(CommandParser.ParsedCommandContainer cmd, Event event) {
+        this.cmd = cmd;
+        this.event = event;
+    }
+
+    public CommandParser.ParsedCommandContainer getCmd() {
+        return cmd;
+    }
+
+    public Event getEvent() {
+        return event;
+    }
+}
---
+++ src/main/java/me/Salt/Event/jevent/CommandPreExecuteEvent.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) 2017 DevJake
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package me.Salt.Event.jevent;
+
+import me.Salt.Command.Container.CommandParser;
+import me.Salt.Event.jevent.util.JEvent;
+import net.dv8tion.jda.core.events.Event;
+
+public class CommandPreExecuteEvent extends JEvent {
+    private CommandParser.ParsedCommandContainer cmd;
+    private Event event;
+    private boolean executionSuccess;
+
+    public CommandPreExecuteEvent(CommandParser.ParsedCommandContainer cmd, Event event, boolean executionSuccess) {
+        this.cmd = cmd;
+        this.event = event;
+        this.executionSuccess = executionSuccess;
+    }
+
+    public CommandParser.ParsedCommandContainer getCmd() {
+        return cmd;
+    }
+
+    public Event getEvent() {
+        return event;
+    }
+
+    public boolean isExecutionSuccess() {
+        return executionSuccess;
+    }
+}
