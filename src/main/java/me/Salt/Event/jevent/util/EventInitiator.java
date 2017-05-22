fc9dfeaf82a8752c031f13cb0568b4904ec3aeeb
Salt001 <salt.exe@gmail.com>
2017-05-22 21:51:43 +0100

    Added event firing system

---
+++ src/main/java/me/Salt/Event/jevent/util/EventInitiator.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2017 DevJake
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package me.Salt.Event.jevent.util;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class EventInitiator {
+    private List<JEventListener> listeners = new ArrayList<>();
+
+    public void fire(JEvent event) {
+        listeners.forEach(l -> l.onEvent(event));
+    }
+
+    public void register(JEventListener listener){
+        listeners.add(listener);
+    }
+
+    public void unregister(JEventListener listener){
+        listeners.remove(listener);
+    }
+}
