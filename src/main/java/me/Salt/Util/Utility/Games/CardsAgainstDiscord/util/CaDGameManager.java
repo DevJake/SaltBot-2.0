bb9e92e71effc9544346fb064f1f4cb26256a8b0
Salt001 <salt.exe@gmail.com>
2017-05-31 01:23:35 +0100

    Further updates to Cards Against Discord

--- src/main/java/me/Salt/Util/Utility/Games/CardsAgainstDiscord/util/CaDGameManager.java
+++ src/main/java/me/Salt/Util/Utility/Games/CardsAgainstDiscord/util/CaDGameManager.java
@@ -20,10 +20,7 @@ import me.Salt.Event.jevent.game.cardsagainstdiscord.CaDManagerInvokeStateEvent;
 import me.Salt.Event.jevent.game.cardsagainstdiscord.CaDManagerModifyHandlerEvent;
 import me.Salt.Event.jevent.game.cardsagainstdiscord.CaDManagerRegisterHandlerEvent;
 import me.Salt.Event.util.EventInitiator;
-import me.Salt.Util.Utility.Games.CardsAgainstDiscord.Entity.BlackCard;
-import me.Salt.Util.Utility.Games.CardsAgainstDiscord.Entity.CaDGameHandler;
-import me.Salt.Util.Utility.Games.CardsAgainstDiscord.Entity.Player;
-import me.Salt.Util.Utility.Games.CardsAgainstDiscord.Entity.WhiteCard;
+import me.Salt.Util.Utility.Games.CardsAgainstDiscord.Entity.*;
 import me.Salt.Util.Utility.Games.GameManager;
 import me.salt.annotations.Metrics;
 import net.dv8tion.jda.core.EmbedBuilder;
@@ -52,6 +49,7 @@ public class CaDGameManager extends ListenerAdapter {
      * A HashMap containing game owner ID's, and their respective {@link CaDGameHandler} game instance
      */
     private static HashMap<String, CaDGameHandler> idLookup = new HashMap<>(); /*User ids and their respective game instance */
+    private static ScheduledExecutorService executorService = new ScheduledThreadPoolExecutor(10);
     
     public static HashMap<String, ResponseContainer> getEmbeds() { return embeds; }
     
@@ -115,6 +113,42 @@ public class CaDGameManager extends ListenerAdapter {
     }
     
     /**
+     * This method takes in the ID of a game owner, and returns the corresponding {@link HandlerContainer} associated
+     * with the game.
+     *
+     * @param ownerId String - The ID of the game's owner
+     * @return {@link HandlerContainer} - The HandlerContainer associated with the specified individual's game instance
+     */
+    public static HandlerContainer getHandlerContainer(String ownerId) {
+        if (idLookup.containsKey(ownerId)) {
+            for (Map.Entry<PlayState, HandlerContainer> containerEntry : handlers) {
+                if (Objects.equals(containerEntry.getValue().getHandler().getOwner().getUser().getId(), ownerId)) {
+                    return containerEntry.getValue();
+                }
+            }
+        }
+        return null;
+    }
+    
+    /**
+     * This method takes in the ID of a game owner, and returns a boolean representing if their game has an
+     * associated {@link HandlerContainer}.
+     *
+     * @param ownerId String - The ID of the game's owner
+     * @return Boolean - If the user has an associated instance of HandlerContainer
+     */
+    public static boolean hasHandlerContainer(String ownerId) {
+        if (idLookup.containsKey(ownerId)) {
+            for (Map.Entry<PlayState, HandlerContainer> containerEntry : handlers) {
+                if (Objects.equals(containerEntry.getValue().getHandler().getOwner().getUser().getId(), ownerId)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    
+    /**
      * This method is called when registering and updating game handlers. It calls upon the
      * {@link HandlerContainer#shouldInvoke()} method to determine if the current handler should be interacted with
      * and - if true - switches through its contained {@link PlayState}. The corresponding methods are then called
@@ -138,11 +172,11 @@ public class CaDGameManager extends ListenerAdapter {
                     case LEADERBOARDS:
                         invokeLeaderboards(containerEntry.getValue().getHandler());
                         break;
-                    case CARD_SELECTION:
-                        invokeCardSelection(containerEntry.getValue().getHandler());
+                    case CZAR_CARD_SELECTION:
+                        invokeCzarCardSelection(containerEntry.getValue().getHandler());
                         break;
-                    case CARD_SUBMITTING:
-                        invokeCardSubmitting(containerEntry.getValue().getHandler());
+                    case USER_CARD_SUBMITTING:
+                        invokeUserCardSubmitting(containerEntry.getValue().getHandler());
                         break;
                 }
             }
@@ -180,30 +214,52 @@ public class CaDGameManager extends ListenerAdapter {
             eb.appendDescription("Be sure not to show anyone else these cards!");
             eb.setFooter("The game will start in 10 seconds", null);
             player.getUser().openPrivateChannel();
-            player.getUser().getPrivateChannel().sendMessage(eb.build()).queue();
+            player.getUser()
+                  .getPrivateChannel()
+                  .sendMessage(eb.build())
+                  .queue(message -> addToEmbeds(message.getId(), ResponseContainer.ResponseExpector.USER_CARD_SELECT,
+                          toHandle));
             // TODO: 27/05/2017 set card czar}
         }
-        ScheduledExecutorService executorService = new ScheduledThreadPoolExecutor(1);
+        //Choosing the Card Czar
+        if (toHandle.getCardCzar() == null) {
+            toHandle.setCardCzar(toHandle.getAllPlayers().get(0)); //Set to first user
+        } else {
+            if (toHandle.getAllPlayers().indexOf(toHandle.getCardCzar()) >= toHandle.getAllPlayers().size())
+                toHandle.setCardCzar(toHandle.getAllPlayers().get(0)); //Reset to first list member
+            else toHandle.setCardCzar(toHandle.getAllPlayers()
+                                              .get(toHandle.getAllPlayers()
+                                                           .indexOf(
+                                                                   toHandle.getCardCzar()))); //Set to next member of list
+        }
+        toHandle.getCardCzar()
+                .getUser()
+                .openPrivateChannel()
+                .queue(privateChannel -> privateChannel.sendMessage(
+                        "You have been marked as this round's Card Czar! In 25 seconds, you will be provided " + "with a list of cards to select from. ")
+                                                       .queue());
+        modifyHandler(toHandle.getOwner().getUser().getId(), PlayState.USER_CARD_SUBMITTING);
+    }
+    
+    /**
+     * This method handles the steps taken for an idle Cards Against Discord game.
+     *
+     * @param toHandle {@link CaDGameHandler} - The current game instance that is being invoked
+     */
+    private static void invokeIdle(CaDGameHandler toHandle) {
+        System.out.println("Invoked IDLE!");
+    }
+    
+    /**
+     * This method handles the steps taken for a Cards Against Discord game in the 'card submission' phase.
+     *
+     * @param toHandle {@link CaDGameHandler} - The current game instance that is being invoked
+     */
+    private static void invokeUserCardSubmitting(CaDGameHandler toHandle) {
+        //Issuing the response embed
+        BlackCard card = CaDUtil.getRandomBlackCard();
+        getHandlerContainer(toHandle.getOwner().getUser().getId()).addRound(new Round(card, toHandle.getCardCzar()));
         executorService.schedule(() -> {
-            if (toHandle.getCardCzar() == null) {
-                toHandle.setCardCzar(toHandle.getAllPlayers().get(0)); //Set to first user
-            } else {
-                if (toHandle.getAllPlayers().indexOf(toHandle.getCardCzar()) >= toHandle.getAllPlayers().size())
-                    toHandle.setCardCzar(toHandle.getAllPlayers().get(0)); //Reset to first list member
-                else toHandle.setCardCzar(toHandle.getAllPlayers()
-                                                  .get(toHandle.getAllPlayers()
-                                                               .indexOf(
-                                                                       toHandle.getCardCzar()))); //Set to next member of list
-            }
-            toHandle.getCardCzar()
-                    .getUser()
-                    .openPrivateChannel()
-                    .queue(privateChannel -> privateChannel.sendMessage(
-                            "You have been marked as this round's Card Czar! In 25 seconds, you will be provided " + "with a list of cards to select from. ")
-                                                           .queue());
-        }, 10, TimeUnit.SECONDS);
-        executorService.schedule(() -> {
-            BlackCard card = CaDUtil.getRandomBlackCard();
             toHandle.addPreviousBlackCard(card);
             toHandle.getAllPlayersNonCzar()
                     .forEach(player -> player.getUser().openPrivateChannel().queue(privateChannel -> {
@@ -254,27 +310,18 @@ public class CaDGameManager extends ListenerAdapter {
                                 }
                             }
                             addToEmbeds(message.getId(), ResponseContainer.ResponseExpector.USER_CARD_SELECT, toHandle);
+                            executorService.schedule(() -> {
+                                getEmbeds().get(message.getId()).setHandled(true);
+                                privateChannel.sendMessage(
+                                        "Selection time is over! The Card Czar is now selecting the winning card.")
+                                              .queue();
+                            }, 15, TimeUnit.SECONDS); //Disabling further adding cards past time limit
                         });
                     }));
-            modifyHandler(toHandle.getOwner().getUser().getId(), PlayState.CARD_SELECTION);
-        }, 35, TimeUnit.SECONDS);
-    }
-    
-    /**
-     * This method handles the steps taken for an idle Cards Against Discord game.
-     *
-     * @param toHandle {@link CaDGameHandler} - The current game instance that is being invoked
-     */
-    private static void invokeIdle(CaDGameHandler toHandle) {
-        System.out.println("Invoked IDLE!");
-    }
-    
-    /**
-     * This method handles the steps taken for a Cards Against Discord game in the 'card submission' phase.
-     *
-     * @param toHandle {@link CaDGameHandler} - The current game instance that is being invoked
-     */
-    private static void invokeCardSubmitting(CaDGameHandler toHandle) {
+            executorService.schedule(
+                    () -> modifyHandler(toHandle.getOwner().getUser().getId(), PlayState.CZAR_CARD_SELECTION), 15,
+                    TimeUnit.SECONDS);
+        }, 10, TimeUnit.SECONDS);
     }
     
     /**
@@ -282,7 +329,64 @@ public class CaDGameManager extends ListenerAdapter {
      *
      * @param toHandle {@link CaDGameHandler} - The current game instance that is being invoked
      */
-    private static void invokeCardSelection(CaDGameHandler toHandle) {
+    private static void invokeCzarCardSelection(CaDGameHandler toHandle) {
+        System.out.println("Handling Czar card selection");
+        //Sending submitted cards to Card Czar
+        System.out.println(getHandlerContainer(toHandle.getOwner().getUser().getId()).getRounds().size());
+        List<Round> rounds = getHandlerContainer(toHandle.getOwner().getUser().getId()).getRounds();
+        if (rounds == null) System.out.println("Rounds = null...??");
+        EmbedBuilder eb1 = new EmbedBuilder();
+        StringBuilder sb1 = new StringBuilder();
+        eb1.setTitle("Choose the winning card!", null);
+        int i = 0;
+        for (CardSubmission cardSubmission : rounds.get(rounds.size()).getCardSubmissions()) {
+            i++;
+            sb1.append("(").append(i).append(") ").append(cardSubmission.getWhiteCard().getText()).append("\n");
+        }
+        eb1.addField("Cards", sb1.toString(), false);
+        System.out.println("Current Card Czar is " + toHandle.getCardCzar().getUser().getName());
+        // TODO: 31/05/2017 Make message actually send, then we are complete
+        toHandle.getCardCzar()
+                .getUser()
+                .openPrivateChannel()
+                .queue(privateChannel -> privateChannel.sendMessage(eb1.build()).queue(message -> {
+                    for (int ii = 0; ii < rounds.get(rounds.size()).getCardSubmissions().size(); ii++) {
+                        switch (ii) {
+                            case 0:
+                                message.addReaction(EmojiManager.getForAlias("one").getUnicode()).queue();
+                                break;
+                            case 1:
+                                message.addReaction(EmojiManager.getForAlias("two").getUnicode()).queue();
+                                break;
+                            case 2:
+                                message.addReaction(EmojiManager.getForAlias("three").getUnicode()).queue();
+                                break;
+                            case 3:
+                                message.addReaction(EmojiManager.getForAlias("four").getUnicode()).queue();
+                                break;
+                            case 4:
+                                message.addReaction(EmojiManager.getForAlias("five").getUnicode()).queue();
+                                break;
+                            case 5:
+                                message.addReaction(EmojiManager.getForAlias("six").getUnicode()).queue();
+                            break;
+                            case 6:
+                                message.addReaction(EmojiManager.getForAlias("seven").getUnicode()).queue();
+                                break;
+                            case 7:
+                                message.addReaction(EmojiManager.getForAlias("eight").getUnicode()).queue();
+                                break;
+                            case 8:
+                                message.addReaction(EmojiManager.getForAlias("nine").getUnicode()).queue();
+                                break;
+                            case 9:
+                                message.addReaction(EmojiManager.getForAlias("keycap_ten").getUnicode()).queue();
+                                break;
+                        }
+                    }
+                    addToEmbeds(message.getId(), ResponseContainer.ResponseExpector.CZAR_CARD_SELECT, toHandle);
+                }));
+        //modifyHandler(toHandle.getOwner().getUser().getId(), PlayState.USER_CARD_SUBMITTING);
     }
     
     /**
@@ -322,11 +426,11 @@ public class CaDGameManager extends ListenerAdapter {
         /**
          * Users are submitting white cards in response to a black card.
          */
-        CARD_SUBMITTING,
+        USER_CARD_SUBMITTING,
         /**
          * The Card Czar is selecting a winning card/card-combo.
          */
-        CARD_SELECTION,
+        CZAR_CARD_SELECTION,
         /**
          * The game is now displaying scoring information to individuals.
          */
