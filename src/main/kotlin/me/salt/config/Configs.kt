743be178d38e1d4f43b31d51da3bfa3779f42de7
Jake <zuezy99@gmail.com>
2017-09-08 13:43:01 +0100

    Fixed an issue where Configs located under entityId's were broken
    The entityId wasn't taken into account of the path to the config, and was instead replaced by 'null'. Issue has now been resolved

--- src/main/kotlin/me/salt/config/Configs.kt
+++ src/main/kotlin/me/salt/config/Configs.kt
@@ -16,6 +16,9 @@
 
 package me.salt.config
 
+import com.fasterxml.jackson.annotation.JsonIgnore
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+import com.fasterxml.jackson.annotation.JsonIgnoreType
 import me.salt.config.entities.Configuration
 import me.salt.exception.ConfigHandlerException
 
@@ -23,81 +26,93 @@ object Configs {
     private var chain = mutableListOf<String>()
     private var entityId: String? = null
 
-    val SALT  = clearAndAdd("SALT")
+    val SALT
+    get() = clearAndAdd("SALT")
 
-    fun GUILD(entityId: String): ConfigTypes = clearAndAdd("USER")
+    fun GUILD(entityId: String): ConfigTypes = clearAndAdd("USER", entityId)
 
-    fun TEXTCHANNEL(entityId: String): ConfigTypes = clearAndAdd("TEXTCHANNEL")
+    fun TEXTCHANNEL(entityId: String): ConfigTypes = clearAndAdd("TEXTCHANNEL", entityId)
 
-    fun VOICECHANNEL(entityId: String): ConfigTypes = clearAndAdd("VOICECHANNEL")
+    fun VOICECHANNEL(entityId: String): ConfigTypes = clearAndAdd("VOICECHANNEL", entityId)
 
-    fun USER(entityId: String): ConfigTypes = clearAndAdd("USER")
+    fun USER(entityId: String): ConfigTypes = clearAndAdd("USER", entityId)
 
     private fun clearAndAdd(termToAdd: String): ConfigTypes {
         chain.clear()
         chain.add(termToAdd)
-        this.entityId = entityId
         return ConfigTypes
+    }
+
+    private fun clearAndAdd(termToAdd: String, entityId: String): ConfigTypes {
+        this.entityId = entityId
+        return clearAndAdd(termToAdd)
     }
 
     object ConfigTypes {
         val MAIN_CONFIG: Handler
             get() {
                 chain.add("MAIN CONFIG")
-                return Handler(chain, entityId)
+                return Handler(chain, entityId).check()
             }
         val LANGUAGE_CONFIG: Handler
             get() {
                 chain.add("LANGUAGE CONFIG")
-                return Handler(chain, entityId)
+                return Handler(chain, entityId).check()
             }
         val LOG_CONFIG: Handler
             get() {
                 chain.add("LOG CONFIG")
-                return Handler(chain, entityId)
+                return Handler(chain, entityId).check()
             }
         val STATS_CONFIG: Handler
             get() {
                 chain.add("STATS CONFIG")
-                return Handler(chain, entityId)
+                return Handler(chain, entityId).check()
             }
 
         val FILTERING_MAP: Handler
             get() {
                 chain.add("FILTERING MAP")
-                return Handler(chain, entityId)
+                return Handler(chain, entityId).check()
             }
         val LEVELLING_MAP: Handler
             get() {
                 chain.add("LEVELLING MAP")
-                return Handler(chain, entityId)
+                return Handler(chain, entityId).check()
             }
         val PERMISSIONS_MAP: Handler
             get() {
                 chain.add("PERMISSIONS MAP")
-                return Handler(chain, entityId)
+                return Handler(chain, entityId).check()
             }
         val SELF_ASSIGNABLE_ROLES_MAP: Handler
             get() {
                 chain.add("SAR MAP")
-                return Handler(chain, entityId)
+                return Handler(chain, entityId).check()
             }
         val STAT_TRACKING_MAP: Handler
             get() {
                 chain.add("STAT TRACKING MAP")
-                return Handler(chain, entityId)
+                return Handler(chain, entityId).check()
+            }
+        val LANGUAGE_MAP: Handler
+            get() {
+                chain.add("LANGUAGE MAP")
+                return Handler(chain, entityId).check()
             }
     }
 
 
 }
 
-class Handler(chain: List<String>, entityId: String?) {
+//@JsonIgnoreType
+class Handler(private val chain: List<String> = emptyList(), private val entityId: String?) {
     var expectedPath = ""
     private var keyword = ""
 
-    init {
-        if (chain.size != 2) throw ConfigHandlerException("The given chain is of the incorrect size(${chain.size})!")
+    fun check() = apply {
+        if (chain.size != 2)
+            throw ConfigHandlerException("The given chain is of the incorrect size(${chain.size})! Chain=$chain")
         when (chain.get(0)) {
             "SALT" -> {
                 expectedPath += "/Admin"
@@ -133,6 +148,7 @@ class Handler(chain: List<String>, entityId: String?) {
             "PERMISSIONS MAP" -> "/Maps/${keyword}PermissionsMap.yaml"
             "SAR MAP" -> "/Maps/${keyword}SARMap.yaml"
             "STAT TRACKING MAP" -> "/Maps/${keyword}StatTrackingMap.yaml"
+            "LANGUAGE MAP" -> "/Maps/${keyword}LanguageMap.yaml"
             else -> throw ConfigHandlerException("The value at chain[1] is incorrect! Must be one of me.salt.config.Configs.ConfigTypes")
         }
 
@@ -142,4 +158,5 @@ class Handler(chain: List<String>, entityId: String?) {
     }
 
     fun <T : Configuration> getConfigFile(configClass: Class<T>): T = ConfigHandler.readConfig(this, configClass)
+    fun updateConfig(config: Configuration) = ConfigHandler.updateConfig(this, config)
 }
