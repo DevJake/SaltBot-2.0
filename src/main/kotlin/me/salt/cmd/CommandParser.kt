93342540ab722bcbb58e1ee39f15071e66ba31a3
Jake <zuezy99@gmail.com>
2017-10-22 00:34:22 +0100

    Continued work on CommandParser#parse

--- src/main/kotlin/me/salt/cmd/CommandParser.kt
+++ src/main/kotlin/me/salt/cmd/CommandParser.kt
@@ -21,13 +21,18 @@ import me.salt.config.entities.GuildConfig
 import me.salt.config.entities.SaltConfig
 import me.salt.config.entities.TextChannelConfig
 import me.salt.config.entities.UserConfig
+import me.salt.events.CommandParseEvent
+import me.salt.events.fireEvent
+import me.salt.exception.AmorphousCommandException
+import me.salt.exception.PrefixlessCommandException
 import me.salt.objects.Entity
 import me.salt.objects.exists
 import me.salt.objects.getConfig
+import java.util.regex.Pattern
 
 object CommandParser {
 
-    fun parse(raw: String, guildId: String, textChannelId: String, userId: String) {
+    fun parse(raw: String, guildId: String, textChannelId: String, userId: String): CommandContainer {
         var saltMain: SaltConfig? = null
         var guildMain: GuildConfig? = null
         var textMain: TextChannelConfig? = null
@@ -45,7 +50,7 @@ object CommandParser {
         if (Configs.user(guildId).MAIN_CONFIG.exists())
             userMain = Configs.user(userId).MAIN_CONFIG.getConfig(UserConfig::class.java)
 
-        var beheaded = mutableMapOf<String, Pair<String, Entity>>()
+        val beheaded = mutableMapOf<Entity, Pair<String, String>>()
 
         /*
         Determines which prefixes are applicable, and filters them into the beheaded variable.
@@ -53,30 +58,50 @@ object CommandParser {
          */
         saltMain?.globalPrefixes?.forEach {
             if (raw.startsWith(it))
-                beheaded.put(it, Pair(raw.replaceFirst(it, ""), Entity.GLOBAL))
+                beheaded.put(Entity.GLOBAL, Pair(it, raw.replaceFirst(it, "")))
         }
 
         guildMain?.guildPrefixes?.forEach {
             if (raw.startsWith(it))
-                beheaded.put(it, Pair(raw.replaceFirst(it, ""), Entity.GUILD))
+                beheaded.put(Entity.GUILD, Pair(it, raw.replaceFirst(it, "")))
         }
 
         textMain?.channelPrefixes?.forEach {
             if (raw.startsWith(it))
-                beheaded.put(it, Pair(raw.replaceFirst(it, ""), Entity.TEXTCHANNEL))
+                beheaded.put(Entity.TEXTCHANNEL, Pair(it, raw.replaceFirst(it, "")))
         }
 
         userMain?.preferredCmdPrefixes?.forEach {
             if (raw.startsWith(it))
-                beheaded.put(it, Pair(raw.replaceFirst(it, ""), Entity.USER))
+                beheaded.put(Entity.USER, Pair(it, raw.replaceFirst(it, "")))
         }
+
+        val beheadedLiteral: String
+
+        if (!Pattern.compile("^[^a-zA-Z]+").matcher(raw.split(" ").get(0)).find())
+            throw PrefixlessCommandException("The given input, $raw, seems to lack any sort of prefix!")
+        if (!Pattern.compile("^.*?[^a-zA-Z]+").matcher(raw.split(" ").get(0)).find())
+            throw AmorphousCommandException("The given input, $raw, has a foul-structured (non-identifiable) prefix!")
+        else
+            beheadedLiteral = raw.replace(Regex("^.*?[^a-zA-Z]+"), "")
+        if (beheaded.isEmpty())
+            throw AmorphousCommandException("The given input, $raw, failed to match any prefixes loaded from configs!")
 
         //TODO If the raw is identified as matching to a command, recursively remove the first character until reaching a letter
         //No specific way to check and appropriately remove identified prefixes without looping over them each removal; inefficient and needless
         //TODO https://github.com/DevJake/SaltBot-2.0/blob/master/src/main/java/me/Salt/Command/Container/CommandParser.java
+        //TODO ensure prefixes cannot **end** with a letter (a-z)
+
+        val cc = CommandContainer(
+                beheaded,
+                beheadedLiteral,
+                raw.split(" "),
+                raw.split(" ").map { it.toLowerCase() }
+        )
+
+        fireEvent(CommandParseEvent(cc))
+        return cc
     }
 
-    class CommandContainer {
-
-    }
+    data class CommandContainer(val beheaded: MutableMap<Entity, Pair<String, String>>, val beheadedLiteral: String, val args: List<String>, val argsLower: List<String>)
 }
