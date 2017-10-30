8fdb4f8413cfc750826c048f6b8f745e9287aa49
Jake <zuezy99@gmail.com>
2017-10-30 20:41:24 +0000

    Updated Copyright headers

--- src/main/kotlin/me/salt/util/rest/ConfigController.kt
+++ src/main/kotlin/me/salt/util/rest/ConfigController.kt
@@ -1,8 +1,25 @@
+/*
+ * Copyright 2017 DevJake
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package me.salt.util.rest
 
 import io.javalin.Context
 import me.salt.entities.config.Configs
 import me.salt.entities.config.entities.SaltConfig
+import me.salt.entities.config.entities.SaltLanguageConfig
 import me.salt.entities.objects.getConfig
 
 //@Controller
