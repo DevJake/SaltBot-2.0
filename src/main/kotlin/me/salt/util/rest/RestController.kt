fcc87a4c3f2d4cef7ea81b385eb111d08a47e737
Jake <zuezy99@gmail.com>
2017-11-02 02:23:33 +0000

    Updated RestController with a duplicate endpoint check

--- src/main/kotlin/me/salt/util/rest/RestController.kt
+++ src/main/kotlin/me/salt/util/rest/RestController.kt
@@ -18,29 +18,39 @@ package me.salt.util.rest
 
 import io.javalin.Context
 import io.javalin.Javalin
+import io.javalin.embeddedserver.Location
 import me.salt.entities.permissions.UserPermission
+import me.salt.util.exception.DuplicateRestEndpointException
+import me.salt.util.exception.exception
 import me.salt.util.logging.logDebug
+import me.salt.util.logging.logInfo
 import me.salt.util.rest.RestController.addDelete
 import me.salt.util.rest.RestController.addGet
 import me.salt.util.rest.RestController.addPatch
 import me.salt.util.rest.RestController.addPost
+import okhttp3.WebSocket
+import okhttp3.WebSocketListener
+import okio.ByteString
 import java.time.OffsetDateTime
 
 object RestController {
-    private val javalin = Javalin.create().port(7000)
+    private val javalin = Javalin.create().port(80)
 
     private val tokens = mutableListOf<AccessToken>().plus(AccessToken("pp487", false, "salt", OffsetDateTime.now(), emptyList(), 0)).toMutableList()
 
+    private val paths = mutableMapOf<String, (Context) -> () -> (Context)>()
+
     init {
         tokens.add(genToken())
+        logInfo("Started Javalin REST API server on port 80", "API")
     }
 
-    fun genToken(oldToken: AccessToken): AccessToken {
+    internal fun genToken(oldToken: AccessToken): AccessToken {
         logDebug("An old REST auth token has been traded for a new one. Old token=$oldToken")
         return genToken()
     }
 
-    fun genToken(): AccessToken {
+    internal fun genToken(): AccessToken {
         return AccessToken("pp486", true, "salt", OffsetDateTime.now(), emptyList(), 0)
     }
 
@@ -54,14 +64,15 @@ object RestController {
         return ctx.status(200)
     }
 
-    fun tokenIsValid(token: String) {}
-    fun tokenIsValid(token: AccessToken) = tokenIsValid(token.token)
+    internal fun tokenIsValid(token: String) {}
+    internal fun tokenIsValid(token: AccessToken) = tokenIsValid(token.token)
 
     internal fun addGet(path: String, call: (Context) -> () -> (Context)) {
         javalin.get("/api" + path, {
             invoke(it, call)
             callLog(it, "get")
         })
+        postInvoke(path, call)
         regLog("/api" + path, "get")
     }
 
@@ -70,6 +81,7 @@ object RestController {
             invoke(it, call)
             callLog(it, "post")
         })
+        postInvoke(path, call)
         regLog("/api" + path, "post")
     }
 
@@ -78,6 +90,7 @@ object RestController {
             invoke(it, call)
             callLog(it, "delete")
         })
+        postInvoke(path, call)
         regLog("/api" + path, "delete")
     }
 
@@ -86,6 +99,7 @@ object RestController {
             invoke(it, call)
             callLog(it, "put")
         })
+        postInvoke(path, call)
         regLog("/api" + path, "put")
     }
 
@@ -94,6 +108,7 @@ object RestController {
             invoke(it, call)
             callLog(it, "patch")
         })
+        postInvoke(path, call)
         regLog("/api" + path, "patch")
     }
 
@@ -102,13 +117,21 @@ object RestController {
             invoke(it, call)
             callLog(it, "trace")
         })
+        postInvoke(path, call)
         regLog("/api" + path, "trace")
     }
 
-    private fun invoke(it: Context, call: (Context) -> () -> (Context)){
+    private fun invoke(it: Context, call: (Context) -> () -> (Context)) {
         val ctx0 = accessManager(it)
         if (!listOf(400, 401, 403).contains(ctx0.status()))
             call.invoke(ctx0).invoke()
+    }
+
+    private fun postInvoke(path: String, call: (Context) -> () -> Context) {
+        if (paths.filter { it.key == path || it.value == call }.isEmpty())
+            paths.put(path, call)
+        else
+            exception(DuplicateRestEndpointException("The endpoint with path $path already shares either the path, function call, or both factors with another endpoint!"))
     }
 
     private fun regLog(path: String, type: String) = logDebug("Registered a new ${type.toUpperCase()} endpoint at path $path")
@@ -137,66 +160,61 @@ data class AccessToken(
 internal fun initRest() {
     addGet("/configs/salt/:type", { ConfigController.getSaltConfigByType(it) })
     addGet("/maps/salt/:type", { ConfigController.getSaltMapByType(it) })
-
     addGet("/configs/guild/:id/:type", { ConfigController.getGuildConfigByType(it) })
     addGet("/maps/guild/:id/:type", { ConfigController.getGuildMapByType(it) })
-
     addGet("/configs/channel/:id/:type", { ConfigController.getChannelConfigByType(it) })
     addGet("/maps/channel/:id/:type", { ConfigController.getChannelMapByType(it) })
-
     addGet("/configs/user/:id/:type", { ConfigController.getUserConfigByType(it) })
     addGet("/maps/user/:id/:type", { ConfigController.getUserMapByType(it) })
-
     addGet("/configs", { ConfigController.getAllConfigs(it) })
     addGet("/maps", { ConfigController.getAllMaps(it) })
-
     addGet("/configs/:type", { ConfigController.getAllConfigsByType(it) })
     addGet("/configs/:category", { ConfigController.getAllConfigsByCategory(it) })
-
     addGet("/maps/:type", { ConfigController.getAllMapsByType(it) })
     addGet("/maps/:category", { ConfigController.getAllMapsByCategory(it) })
-
     addGet("/configs/search/:type", { ConfigController.searchAllConfigsByType(it) })
     addGet("/maps/search/:type", { ConfigController.searchAllMapsByType(it) })
-
     addGet("/configs/search/:category/:type", { ConfigController.searchAllConfigsByCategoryAndType(it) })
     addGet("/maps/search/:category/:type", { ConfigController.searchAllMapsByCategoryAndType(it) })
 
-    addDelete("/configs/salt/:type", { ConfigController.deleteSaltConfigByType(it) })
-    addDelete("/maps/salt/:type", { ConfigController.deleteSaltMapByType(it) })
-
-    addDelete("/configs/guild/:id/:type", { ConfigController.deleteGuildConfigByType(it) })
-    addDelete("/maps/guild/:id/:type", { ConfigController.deleteGuildMapByType(it) })
-
-    addDelete("/configs/channel/:id/:type", { ConfigController.deleteChannelConfigByType(it) })
-    addDelete("/maps/channel/:id/:type", { ConfigController.deleteChannelMapByType(it) })
-
-    addDelete("/configs/user/:id/:type", { ConfigController.deleteUserConfigByType(it) })
-    addDelete("/maps/user/:id/:type", { ConfigController.deleteUserMapByType(it) })
+    addDelete("/configs/salt/:type/delete", { ConfigController.deleteSaltConfigByType(it) })
+    addDelete("/maps/salt/:type/delete", { ConfigController.deleteSaltMapByType(it) })
+    addDelete("/configs/guild/:id/:type/delete", { ConfigController.deleteGuildConfigByType(it) })
+    addDelete("/maps/guild/:id/:type/delete", { ConfigController.deleteGuildMapByType(it) })
+    addDelete("/configs/channel/:id/:type/delete", { ConfigController.deleteChannelConfigByType(it) })
+    addDelete("/maps/channel/:id/:type/delete", { ConfigController.deleteChannelMapByType(it) })
+    addDelete("/configs/user/:id/:type/delete", { ConfigController.deleteUserConfigByType(it) })
+    addDelete("/maps/user/:id/:type/delete", { ConfigController.deleteUserMapByType(it) })
 
     addPost("/configs/salt/:type/reset", { ConfigController.resetSaltConfigByType(it) })
     addPost("/maps/salt/:type/reset", { ConfigController.resetSaltMapByType(it) })
-
     addPost("/configs/guild/:id/:type/reset", { ConfigController.resetGuildConfigByType(it) })
     addPost("/maps/guild/:id/:type/reset", { ConfigController.resetGuildMapByType(it) })
-
     addPost("/configs/channel/:id/:type/reset", { ConfigController.resetChannelConfigByType(it) })
     addPost("/maps/channel/:id/:type/reset", { ConfigController.resetChannelMapByType(it) })
-
     addPost("/configs/user/:id/:type/reset", { ConfigController.resetUserConfigByType(it) })
     addPost("/maps/user/:id/:type/reset", { ConfigController.resetUserMapByType(it) })
 
     addPatch("/configs/salt/:type/patch", { ConfigController.patchSaltConfigByType(it) })
     addPatch("/maps/salt/:type/patch", { ConfigController.patchSaltMapByType(it) })
-
     addPatch("/configs/guild/:id/:type/patch", { ConfigController.patchGuildConfigByType(it) })
     addPatch("/maps/guild/:id/:type/patch", { ConfigController.patchGuildMapByType(it) })
-
     addPatch("/configs/channel/:id/:type/patch", { ConfigController.patchChannelConfigByType(it) })
     addPatch("/maps/channel/:id/:type/patch", { ConfigController.patchChannelMapByType(it) })
-
     addPatch("/configs/user/:id/:type/patch", { ConfigController.patchUserConfigByType(it) })
     addPatch("/maps/user/:id/:type/patch", { ConfigController.patchUserMapByType(it) })
 
-    //TODO /maps/salt/...
+    addGet("/logs/any/:id", { LogController.getLogById(it) })
+    addGet("/logs/salt/:id/:datetime", { LogController.getSaltLogByIdAndDateTime(it) })
+    addGet("/logs/guild/:id/:datetime", { LogController.getGuildLogByIdAndDateTime(it) })
+    addGet("/logs/voicechannel/:id/:datetime", { LogController.getVoiceChannelLogByIdAndDateTime(it) })
+    addGet("/logs/channel/:id/:datetime", { LogController.getChannelLogByIdAndDateTime(it) })
+    addGet("/logs/user/:id/:datetime", { LogController.getUserLogByIdAndDateTime(it) })
+    addGet("/logs/salt/:id/:entry", { LogController.getSaltLogByIdAndEntryType(it) })
+    addGet("/logs/guild/:id/:entry", { LogController.getGuildLogByIdAndEntryType(it) })
+    addGet("/logs/voicechannel/:id/:entry", { LogController.getVoiceChannelLogByIdAndEntryType(it) })
+    addGet("/logs/channel/:id/:entry", { LogController.getChannelLogByIdAndEntryType(it) })
+    addGet("/logs/user/:id/:entry", { LogController.getUserLogByIdAndEntryType(it) })
+
+    //TODO replace paths with :id/:entry/:datetime/:etc with form param calls. Therefore also replace duplicate check with form param check (through bool passed as addGet() param)
 }
