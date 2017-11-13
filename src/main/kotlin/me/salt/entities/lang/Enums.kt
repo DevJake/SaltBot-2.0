adac9a1a63163d9ce9b25c10eea0935953a58208
Jake <zuezy99@gmail.com>
2017-11-13 12:51:51 +0000

    Added a lot more LangCodes, Languages and Countries to Enums.kt

--- src/main/kotlin/me/salt/entities/lang/Enums.kt
+++ src/main/kotlin/me/salt/entities/lang/Enums.kt
@@ -22,21 +22,45 @@ import me.salt.entities.objects.getConfig
 
 /*
 A range of default languages defined in SaltLanguageMap.yaml
+
+Source: http://quivi.sourceforge.net/languagecodes.html
  */
-enum class LangCode(val language: Language, val country: Country) {
+enum class LangCode(val language: Language, vararg val country: Country?) {
+    EN(Language.ENGLISH),
     EN_GB(Language.ENGLISH, Country.GREAT_BRITAIN),
-    EN_US(Language.ENGLISH, Country.UNITED_STATES);
+    EN_US(Language.ENGLISH, Country.UNITED_STATES),
+    AF_ZA(Language.AFRIKAANS, Country.SOUTH_AFRICA, Country.NAMIBIA),
+    SQ_AL(Language.ALBANIAN, Country.ALBANIA),
+    AR(Language.ARABIC),
+    AR_DZ(Language.ARABIC, Country.ALGERIA),
+    AR_BH(Language.ARABIC, Country.BAHRAIN),
+    AR_EG(Language.ARABIC, Country.EGYPT),
+    AR_IQ(Language.ARABIC, Country.IRAQ),
+    AR_JO(Language.ARABIC, Country.JORDAN),
+    AR_KW(Language.ARABIC, Country.KUWAIT),
+    AR_LB(Language.ARABIC, Country.LEBANON),
+    AR_LY(Language.ARABIC, Country.LIBYA),
+    AR_MA(Language.ARABIC, Country.MOROCCO),
+    AR_OM(Language.ARABIC, Country.OMAN),
+    AR_QA(Language.ARABIC, Country.QATAR),
+    AR_SA(Language.ARABIC, Country.SAUDI_ARABIA),
+    AR_SD(Language.ARABIC, Country.SUDAN),
+    AR_SY(Language.ARABIC, Country.SYRIA),
+    AR_TN(Language.ARABIC, Country.TUNISIA),
+    AR_AE(Language.ARABIC, Country.UNITED_ARAB_EMIRATES),
+    AR_YE(Language.ARABIC, Country.YEMEN);
+
 
     fun getLang() = Configs.salt.LANGUAGE_MAP.getConfig(LanguageMap::class.java)?.getLanguage(this.name)
 
     fun filterBy(language: Language, country: Country, inverse: Boolean = false) =
-            LangCode.values().toList().filter { (it.language == language && it.country == country) != inverse }
+            LangCode.values().toList().filter { (it.language == language && it.country.contains(country)) != inverse }
 
     fun filterBy(language: Language, inverse: Boolean = false) =
             LangCode.values().toList().filter { (it.language == language) != inverse }
 
     fun filterBy(country: Country, inverse: Boolean = false) =
-            LangCode.values().toList().filter { (it.country == country) != inverse }
+            LangCode.values().toList().filter { (it.country.contains(country)) != inverse }
 }
 
 enum class Language {
@@ -45,7 +69,10 @@ enum class Language {
     RUSSIAN,
     GERMAN,
     SPANISH,
-    MANDARIN;
+    MANDARIN,
+    AFRIKAANS,
+    ALBANIAN,
+    ARABIC,
 }
 
 enum class Country {
@@ -55,7 +82,27 @@ enum class Country {
     GERMANY,
     RUSSIA,
     CHINA,
-    SPAIN;
+    SPAIN,
+    SOUTH_AFRICA,
+    NAMIBIA,
+    ALBANIA,
+    ALGERIA,
+    EGYPT,
+    BAHRAIN,
+    IRAQ,
+    KUWAIT,
+    JORDAN,
+    LEBANON,
+    LIBYA,
+    SYRIA,
+    UNITED_ARAB_EMIRATES,
+    YEMEN,
+    MOROCCO,
+    OMAN,
+    QATAR,
+    SAUDI_ARABIA,
+    SUDAN,
+    TUNISIA
 }
 
 enum class LangTerm(vararg val variables: Variable?) {
@@ -80,18 +127,38 @@ enum class LangTerm(vararg val variables: Variable?) {
     USER_LACKS_AUTHORITY(
             Variable("level", "Authority Level", "The level of the authority"),
             Variable("interaction", "Authority Interaction type", "The type of interaction for the authority")),
-    CONFIG_CREATE_SUCCESS(),
-    CONFIG_CREATE_FAILURE(),
-    CONFIG_RESET_SUCCESS(),
-    CONFIG_RESET_FAILURE(),
-    CONFIG_RESTORE_SUCCESS(), //TODO allow restoration of configs to version prior to edit. Useful if a mistake is made and requires a rollback
-    CONFIG_RESTORE_FAILURE(),
-    CONFIG_UPDATE_SUCCESS(),
-    CONFIG_UPDATE_FAILURE(),
-    CONFIG_RETRIEVE_SUCCESS(),
-    CONFIG_RETRIEVE_FAILURE(),
-    EXPECTED_EXCEPTION(), //Code was aware and prepared for the event the exception was thrown. Common in errors in user input
-    UNEXPECTED_EXCEPTION(); //Code didn't anticipate the exception; common in unchecked exceptions, caused commonly by internal code errors and mischecks
+
+    EXPECTED_EXCEPTION_DEFAULT(), //Code was aware and prepared for the event the exception was thrown. Common in errors in user input
+    UNEXPECTED_EXCEPTION_DEFAULT(), //Code didn't anticipate the exception; common in unchecked exceptions, caused commonly by internal code errors and mischecks
+
+    COMMAND_HELLO_WORLD(
+            Variable("user", "User's nickname", "The nickname of the command sender"),
+            Variable("time", "Event time", "The time at which the command was received")),
+    COMMAND_PERMS_USER_HAS_PERM(),
+    COMMAND_PERMS_USER_LACKS_PERM(),
+    COMMAND_PERMS_ADD_PERM_USER_SUCCESS(),
+    COMMAND_PERMS_ADD_PERM_USER_FAILURE(),
+    COMMAND_PERMS_ADD_PERM_GROUP_SUCCESS(),
+    COMMAND_PERMS_ADD_PERM_GROUP_FAILURE(),
+
+    COMMAND_PERMS_REMOVE_PERM_USER_SUCCESS(),
+    COMMAND_PERMS_REMOVE_PERM_USER_FAILURE(),
+    COMMAND_PERMS_REMOVE_PERM_GROUP_SUCCESS(),
+    COMMAND_PERMS_REMOVE_PERM_GROUP_FAILURE(),
+
+    COMMAND_CONFIG_CREATE_SUCCESS(),
+    COMMAND_CONFIG_CREATE_FAILURE(),
+    COMMAND_CONFIG_RESET_SUCCESS(),
+    COMMAND_CONFIG_RESET_FAILURE(),
+    COMMAND_CONFIG_RESTORE_SUCCESS(), //TODO allow restoration of configs to version prior to edit. Useful if a mistake is made and requires a rollback
+    COMMAND_CONFIG_RESTORE_FAILURE(),
+    COMMAND_CONFIG_UPDATE_SUCCESS(),
+    COMMAND_CONFIG_UPDATE_FAILURE(),
+    COMMAND_CONFIG_RETRIEVE_SUCCESS(),
+    COMMAND_CONFIG_RETRIEVE_FAILURE();
+
+
+
 
     //TODO params should be wrapped with double asterisks, i.e., **user**
     data class Variable(var term: String, var name: String, var description: String? = null)
